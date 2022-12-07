1a36efbf0ae25988007e362f70adc7b7b47c592f
Brymmobaggins <ibro2tablet21@yahoo.com>
2022-12-07 14:52:36 +0100

    Add sum of natural numbers

---
+++ flow-of-control/set-2/SumNatural.cpp
@@ -0,0 +1,17 @@
+// Write a program to calculate the sum of first 10 natural number
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+      
+    int sum = 0;
+    for(int i = 1; i <= 10; i++){
+        sum += i;
+    }
+    cout << "Sum: " <<sum << endl;
+
+    return 0;    
+}
+// Sum : 55
