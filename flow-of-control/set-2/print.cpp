b028488313e39ac484e64c929fd93158e206b816
Brymmobaggins <ibro2tablet21@yahoo.com>
2022-12-07 12:09:58 +0100

    delete prject and comment few

--- flow-of-control/set-2/print.cpp
+++ flow-of-control/set-2/print.cpp
@@ -1,13 +1,15 @@
 // Write a program to print number from 1 to 10.
 
-
 #include <iostream>
 using namespace std;
 
-int main(){
-     int x
-    for()
+int main()
+{
 
-
-
+    cout << " The first 10 numbers are as folows: \n";
+     for (int i = 1; i <= 10; i++)
+    {
+        cout << i << endl;
+    }
+    return 0;
 }
