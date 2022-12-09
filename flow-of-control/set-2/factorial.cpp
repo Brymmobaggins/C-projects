bf19fcdf62f4941e89842a0c2b35e768c5dbd64b
Brymmobaggins <ibro2tablet21@yahoo.com>
2022-12-09 14:44:28 +0100

    add commfactor

--- flow-of-control/set-2/factorial.cpp
+++ flow-of-control/set-2/factorial.cpp
@@ -1,16 +1,21 @@
 // Write a program to find the factorial value of any number entered through the keyboard
 
-#include <iostream>
-
+#include <iostream> 
 using namespace std;
 
-int main(){
+int main()
+{
+    int n, fact = 1;
+    cout << "Enter any number : ";
+    cin >> n;
 
-    int number;
-    cout << "Enter any number: ";
-    cin >> number;
+    while (n >= 1)
+    {
+        fact *= n;
+        n--;
+    }
 
-    
+    cout << "Factorial :" << fact;
 
-
+    return 0;
 }
