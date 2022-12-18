915191e2ecb33990b42b1dd9400afaed90a58274
Brymmobaggins <ibro2tablet21@yahoo.com>
2022-12-18 13:00:50 +0100

    update string length code

--- strings/length-of-string.cpp
+++ strings/length-of-string.cpp
@@ -11,35 +11,36 @@ using namespace std;
 //     cout << str.uuu
 
 // size of string object using for loop method
-// int main( )
-// {
-//     char str[80];
+int main( )
+{
+    char str[80];
 
-//     cout<<"Enter string: ";
-//     cin.getline(str, 80);
+    cout<<"Enter string: ";
+    cin.getline(str, 80);
     
-//     int i; //Hold length of string
+    int i; //Hold length of string
     
-//     for(i = 0; str[i] != '\0'; i++);
+    for(i = 0; str[i] != '\0'; i++);
 
-//     cout << "Length of string is: " << i;
+    cout << "Length of string is: " << i;
 	
-//     return 0;
-// }
+    return 0;
+}
 
 // size of string object using size method
+int main(){
 
-// int main(){
+    string x;
+    cout << "Enter String: ";
+    cin >> x;
 
-//     string x;
-//     cout << "Enter String: ";
-//     cin >> x;
-
-//     cout <<" The length of the string is: " << x.size() << endl;
+    cout <<" The length of the string is: " << x.size() << endl;
 
 
-//     return 0;
-// }
+    return 0;
+}
+
+
 
 // size of string object using length method
 int main(){
